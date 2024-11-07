3646b3e436e70c82bc9a134ddf0b8b5be39b4a19
canonvip <114199886+canonvip@users.noreply.github.com>
2024-11-07 22:47:17 +0800

    Update test.js

--- test.js
+++ test.js
@@ -4,9 +4,11 @@
 # 商品id获取, 查看商品详情触发通知
 https:\/\/.+\.jd\.com\/graphext\/draw\?sku=(\d+).* url script-request-header https://raw.githubusercontent.com/id77/QuantumultX/master/Script/jdapp_to_union.js
 https:\/\/.+\.jd\.com\/product\/.*\/(\d+)\.html url script-request-header https://raw.githubusercontent.com/id77/QuantumultX/master/Script/jdapp_to_union.js
+
 [mitm]
 hostname = *.jd.com, *.*.jd.com
 */
+
 // 输出捕获的请求 URL
 console.log(`🔗 捕获：\n${$request.url}`);
 
@@ -16,17 +18,13 @@ let appType = UA.match(/(.+?);/)[1];  // 获取应用类型
 let sku;
 let arr = [];
 
-
 // 根据不同的 URL 模式匹配 SKU
 if (url.includes('graphext/draw')) {
-    // 如果 URL 包含 'graphext/draw'，则从中提取 SKU
     arr = url.match(/sku=(\d+)/);
     appType = 'jdpingou';  // 设置应用类型为 'jdpingou'
 } else if (url.includes('wqsitem.jd.com/detail')) {
-    // 如果 URL 包含 'wqsitem.jd.com/detail'，提取 SKU
     arr = url.match(/wqsitem\.jd\.com\/detail\/(\d+)_/);
 } else {
-    // 默认匹配 '/xxx/xxx/xxx/123456.html' 类型的 URL，提取 SKU
     arr = url.match(/\/.*\/(\d+)\.html/);
 }
 
