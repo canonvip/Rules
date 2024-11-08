08967179a8eb687a47ff5261f2ad081e47bc3157
canonvip <114199886+canonvip@users.noreply.github.com>
2024-11-08 10:43:59 +0800

    Update test.js

--- test.js
+++ test.js
@@ -10,9 +10,9 @@ hostname = *.jd.com, *.*.jd.com
 */
 const chen = new Env('')//init()
 // 获取 AppId、AppKey 和 UnionId
-const AppId = chen.getData('shine_jingPinKU_AppId');
-const AppKey = chen.getData('shine_jingPinKU_AppKey');
-const UnionId = chen.getData('shine_jingPinKU_UnionId');
+const AppId = chen.getdata('shine_jingPinKU_AppId');
+const AppKey = chen.getdata('shine_jingPinKU_AppKey');
+const UnionId = chen.getdata('shine_jingPinKU_UnionId');
 console.log(`👾 AppId：${AppId}`);
 console.log(`👾 AppKey：${AppKey}`)
 console.log(`👾 UnionId：${UnionId}`)
